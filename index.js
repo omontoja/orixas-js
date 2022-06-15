1857d8a2112efd3a011e1fdeee02383674031d68
Antonio Montoja <61279163+omontoja@users.noreply.github.com>
2022-06-15 10:52:40 -0300

    final-update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var numeroRecebido = "99/99/9999";
+var numeroRecebido = "11/06/1999";
 var numeroPar = 0;
 var numeroImpar = 0;
 var lista = [];
-
+var fronteDireita = 0;
+var fronteEsquerda = 0;
+var centro = 0;
 
 function soNumeros() {
     numeroRecebido = numeroRecebido.replace(/[^0-9]/g, '');
@@ -15,21 +17,71 @@ for (var i in lista) {
     lista[i] = parseInt(lista[i]);
 }
 
-for (var i in lista) {
-    if (i % 2 === 0) {
-        numeroPar += lista[i];
-    } else {
-        numeroImpar += lista[i];
+function somaNuca() {
+    for (var i in lista) {
+        if (i % 2 === 0) {
+            numeroPar += lista[i];
+        }
+    }
+    if (numeroPar > 16) {
+        numeroPar = numeroPar.toString().split("");
+        numeroPar = parseInt(numeroPar[0]) + parseInt(numeroPar[1]);
     }
 }
-if (numeroImpar > 16) {
 
-    numeroImpar = numeroImpar.toString().split("");
-    numeroImpar = parseInt(numeroImpar[0]) + parseInt(numeroImpar[1]);
-    console.log("A soma dos numeros impares é " + numeroImpar);
+function somaTesta() {
+    for (var i in lista) {
+        if (i % 2 === 1) {
+            numeroImpar += lista[i];
+        }
+    }
+    if (numeroImpar > 16) {
+        numeroImpar = numeroImpar.toString().split("");
+        numeroImpar = parseInt(numeroImpar[0]) + parseInt(numeroImpar[1]);
+    }
 }
-if (numeroPar > 16) {
-    numeroPar = numeroPar.toString().split("");
-    numeroPar = parseInt(numeroPar[0]) + parseInt(numeroPar[1]);
-    console.log("A soma dos numeros pares é " + numeroPar);
+
+function somaFronteDireita() {
+    fronteDireita = numeroPar + numeroImpar;
+
+    if (fronteDireita > 16) {
+        fronteDireita = fronteDireita.toString().split("");
+        fronteDireita = parseInt(fronteDireita[0]) + parseInt(fronteDireita[1]);
+    }
 }
+
+function somaFronteEsquerda() {
+    fronteEsquerda = numeroImpar + numeroPar + fronteDireita;
+
+    if (fronteEsquerda > 16) {
+        fronteEsquerda = fronteEsquerda.toString().split("");
+        fronteEsquerda = parseInt(fronteEsquerda[0]) + parseInt(fronteEsquerda[1]);
+    }
+}
+
+function somaCentro() {
+    for (var i in lista) {
+        centro += lista[i];
+    }
+
+    if (centro > 16) {
+        centro = centro.toString().split("");
+        centro = parseInt(centro[0]) + parseInt(centro[1]);
+    }
+}
+
+function calcularOrixas() {
+    somaNuca();
+    somaTesta();
+    somaFronteDireita();
+    somaFronteEsquerda();
+    somaCentro();
+}
+
+calcularOrixas();
+
+console.log("Nuca: " + numeroPar);
+console.log("Testa: " + numeroImpar);
+console.log("Fronte direita: " + fronteDireita);
+console.log("Fronte esquerda: " + fronteEsquerda);
+console.log("Centro: " + centro);
